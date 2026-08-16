67fa81208ba71cd4a20dc19cc0c3707836b28a76
ANUSMITA JHA <anusmitajha@ANUSMITAs-MacBook-Air.local>
2026-08-16 20:25:14 +0530

    Add branch filter and POST student API

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const students = [
     { id: 1, name: "Aarav", branch: "CSE" },
     { id: 2, name: "Diya", branch: "ECE" },
@@ -15,14 +17,43 @@ app.get("/students", (req, res) => {
     res.json(students);
 });
 
-app.get("/students/:id", (req, res) => {
-    const student = students.find(s => s.id === parseInt(req.params.id));
-    if (student) {
-        res.json(student);
+app.get("/students/branch/:branch", (req, res) => {
+    const branch = req.params.branch.toUpperCase();
+
+    const filteredStudents = students.filter(
+        s => s.branch.toUpperCase() === branch
+    );
+
+    if (filteredStudents.length > 0) {
+        res.json(filteredStudents);
     } else {
-        res.status(404).json({ error: "Student not found" });
+        res.status(404).json({
+            error: "No students found in this branch"
+        });
     }
 });
+
+
+app.post("/students", (req, res) => {
+    const { name, branch } = req.body;
+
+    if (!name || !branch) {
+        return res.status(400).json({
+            error: "Name and branch are required"
+        });
+    }
+
+    const newStudent = {
+        id: students.length + 1,
+        name: name,
+        branch: branch
+    };
+
+    students.push(newStudent);
+
+    res.status(201).json(newStudent);
+});
+
 
 app.listen(3000, () => {
     console.log("Server started at http://localhost:3000");
